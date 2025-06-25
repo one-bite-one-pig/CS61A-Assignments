931074a0d6920ef6ae336c3b24a97f314bb95eca
one-bite-one-pig <2071618869@qq.com>
2025-06-25 20:17:24 +0800

    lab03-finished

--- Lab/lab03/lab03.py
+++ Lab/lab03/lab03.py
@@ -15,6 +15,8 @@ def print_if(s, f):
     """
     for x in s:
         "*** YOUR CODE HERE ***"
+        if f(x):
+            print(x)
 
 
 def close(s, k):
@@ -33,6 +35,8 @@ def close(s, k):
     count = 0
     for i in range(len(s)):  # Use a range to loop over indices
         "*** YOUR CODE HERE ***"
+        if abs(s[i]-i)<=k:
+            count+=1
     return count
 
 
@@ -47,7 +51,7 @@ def close_list(s, k):
     >>> close_list(t, 2)  # 2, 3, 4, and 5 are all within 2 of their index
     [2, 4, 3, 5]
     """
-    return [___ for i in range(len(s)) if ___]
+    return [s[i] for i in range(len(s)) if abs(s[i]-i)<=k]
 
 
 from math import sqrt
@@ -63,13 +67,12 @@ def squares(s):
     >>> squares(seq)
     []
     """
-    return [___ for n in s if ___]
+    return [int(sqrt(n)) for n in s if sqrt(n)%1==0]
 
 
 def double_eights(n):
     """Returns whether or not n has two digits in row that
     are the number 8.
-
     >>> double_eights(1288)
     True
     >>> double_eights(880)
@@ -88,6 +91,19 @@ def double_eights(n):
     True
     """
     "*** YOUR CODE HERE ***"
+    if(n<100):
+        if n==88 :
+            return True
+        else:
+            return False
+    else:
+        t=n%100
+        r=n//10
+        if(t==88):
+            return True
+        else:
+            return double_eights(r)
+    
 
 
 def make_onion(f, g):
@@ -116,10 +132,10 @@ def make_onion(f, g):
     """
     def can_reach(x, y, limit):
         if limit < 0:
-            return ____
+            return False
         elif x == y:
-            return ____
+            return True
         else:
-            return can_reach(____, ____, limit - 1) or can_reach(____, ____, limit - 1)
+            return can_reach(f(x), y, limit - 1) or can_reach(g(x), y, limit - 1)
     return can_reach
 
