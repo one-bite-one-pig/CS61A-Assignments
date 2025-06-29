f69cd73ebe43b268869a4906204ee4488716b507
one-bite-one-pig <2071618869@qq.com>
2025-06-30 00:47:39 +0800

    lab09-finished

--- Lab/lab09/lab09.scm
+++ Lab/lab09/lab09.scm
@@ -1,10 +1,27 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2)
+        (cond((> num1 num2) 1)
+             ((= num1 num2) 0)
+             (else          -1)        
 
-(define (make-adder num) 'YOUR-CODE-HERE)
+        )
+)
 
-(define (composed f g) 'YOUR-CODE-HERE)
+(define (make-adder num) 
+        (lambda(x)(+ x num))
 
-(define (repeat f n) 'YOUR-CODE-HERE)
+)
+
+(define (composed f g) 
+        (lambda(x)(f (g x)))
+)
+
+(define (repeat f n) (
+        if (= n 1) (lambda(x)(f x))
+        (composed f (repeat f (- n 1)))
+         
+      
+)
+)
 
 (define (max a b)
   (if (> a b)
@@ -16,4 +33,20 @@
       b
       a))
 
-(define (gcd a b) 'YOUR-CODE-HERE)
+(define (gcd a b) (
+        cond 
+          ((= b 0) a)
+          (else (gcd (min a b) (modulo (max a b) ( min a b)) ))
+
+
+
+
+
+
+)
+
+
+
+
+
+)
