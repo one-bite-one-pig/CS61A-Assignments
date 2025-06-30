ee6f692cd71a0c2f40c016a8be723370f92b2a6e
one-bite-one-pig <2071618869@qq.com>
2025-06-30 15:44:38 +0800

    hw09-finished

--- HW/hw08/hw08.scm
+++ HW/hw08/hw08.scm
@@ -1,7 +1,63 @@
-(define (ascending? s) 'YOUR-CODE-HERE)
+(
+    define (ascending? l)(
+        if (or (null? l) (null? (cdr l)))
+        #t
+        (
+            if (<= (car l) (car (cdr l)))
+                (ascending? (cdr l))
+            #f
+        )
 
-(define (my-filter pred s) 'YOUR-CODE-HERE)
 
-(define (interleave lst1 lst2) 'YOUR-CODE-HERE)
 
-(define (no-repeats s) 'YOUR-CODE-HERE)
+    ) 
+)
+
+(
+    define (my-filter pred s) (
+        cond ((null? s)  nil)
+             ((null? (cdr s))(
+                if (pred (car s))
+                    s
+                    nil
+             ))
+            (else(
+                if(pred (car s))
+                    (cons ( car s)  (my-filter pred (cdr s)))
+                    (my-filter pred (cdr s))
+             ))
+        
+    )
+    
+    
+    
+)
+
+(
+    define (interleave lst1 lst2) (
+        cond ((null? lst1)  lst2)
+            ((null? lst2)   lst1)
+            (
+                else(cons (car lst1) (interleave lst2 (cdr lst1)))
+            )
+    )
+)
+
+(
+    define (no-repeats s)(
+        if (null? s) s
+        (
+            cons  (car s)  (no-repeats (filter  (lambda(x) (not (= x (car s)))  )  (cdr s)    ))
+        )
+
+
+
+
+
+
+    ) 
+
+
+
+
+)
