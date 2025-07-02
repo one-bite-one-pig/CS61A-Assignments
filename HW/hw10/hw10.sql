ec281cf402b131b1bb10ab48973872479ae92d6a
one-bite-one-pig <2071618869@qq.com>
2025-07-02 17:14:41 +0800

    hw10-finished

--- HW/hw10/hw10.sql
+++ HW/hw10/hw10.sql
@@ -26,24 +26,25 @@ CREATE TABLE sizes AS
 
 -- All dogs with parents ordered by decreasing height of their parent
 CREATE TABLE by_parent_height AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT child AS chill FROM parents,dogs AS pup, dogs AS father WHERE pup.name=child  AND father.name =parent Order by -father.height ;
 
 
 -- The size of each dog
 CREATE TABLE size_of_dogs AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT name,size from dogs,sizes where   height>min and height<=max;
 
 
 -- [Optional] Filling out this helper table is recommended
 CREATE TABLE siblings AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT little.child AS small,big.child AS large FROM parents AS little, parents AS big,dogs AS l,dogs AS b where little.parent=big.parent and l.name< b.name and l.name=little.child and b.name=big.child;
 
 -- Sentences about siblings that are the same size
 CREATE TABLE sentences AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
-
+  SELECT "The two siblings, "||small||" and "||large||", have the same size: "||little.size from siblings,size_of_dogs AS little, size_of_dogs AS big where small=little.name and large=big.name and little.size=big.size
+  ;
+s
 
 -- Height range for each fur type where all of the heights differ by no more than 30% from the average height
 CREATE TABLE low_variance AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT fur,max(height)-min(height) from dogs group by fur having avg(height)*1.3>=max(height) and avg(height)*0.7 <=min(height);
 
